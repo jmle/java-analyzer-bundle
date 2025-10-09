874f49409385832f9cfcb68083ab6eca3b4dd829
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2025-10-09 11:02:57 +0200

    Add filters
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- export-index/src/main/java/Artifact.java
+++ export-index/src/main/java/Artifact.java
@@ -1,6 +1,28 @@
 import org.apache.lucene.document.Document;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Artifact {
+
+    private static final Set<String> SKIPPED_CLASSIFIERS =
+            new HashSet<>(Arrays.asList(("javadoc javadocs docs groovydoc site"
+                    + " source sources src source-release project-src gf-project-src"
+                    + " test tests test-sources tests-sources test-javadoc tests-javadoc"
+                    + " maven-archetype maven-plugin"
+                    + " bin app bundle image dist distribution assembly resources scripts"
+                    + " module kubernetes openshift helm").split(" ")));
+
+    private final Set<String> SKIPPED_PACKAGINGS  = new HashSet<>(Arrays.asList(
+            ("png eclipse-repository xhtml ${packaging.type} ${lifecycle} ${packaging}"
+                    + " jbi-service-unit eclipse-test-plugin atlassian-plugin sh cfg list tree jszip"
+                    + " pdf eclipse-feature eclipse-plugin swf jangaroo swc html xsd txt apk jdocbook nexus-plugin"
+                    + " sonar-plugin nbm yml maven-archetype maven-plugin").split(" ")));
+
+    private final Set<String> SKIP_VERSIONS_CONTAINING  = new HashSet<>(Arrays.asList(
+            "rc alpha beta snap [-,\\.]cr [-,\\.]pre".split(" ")));
+
     private String sha;
     private String groupId;
     private String artifactId;
@@ -51,7 +73,10 @@ public class Artifact {
 
     public boolean shouldBeIndexed() {
         // All that we don't need can be filtered out here
-        return groupId != null && !classifier.equals("sources") && !classifier.equals("javadocs") && !classifier.equals("tests");
+        return groupId != null
+                && !SKIPPED_CLASSIFIERS.contains(classifier)
+                && !SKIPPED_PACKAGINGS.contains(packaging)
+                && SKIP_VERSIONS_CONTAINING.stream().noneMatch(ver -> version.matches(String.format("(?i).*%s.*", ver)));
     }
 
     public static Artifact fromDocument(Document d) {
